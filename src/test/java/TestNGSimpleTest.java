8099c306dbb1a28667ca11887c3a937b415e406a
Nicholas Inns <Nick.Inns@veritas.com>
2017-09-28 13:12:41 -0700

    Test

--- src/test/java/TestNGSimpleTest.java
+++ src/test/java/TestNGSimpleTest.java
@@ -6,4 +6,40 @@ public class TestNGSimpleTest {
     public void testOne(){
         assertEquals("Hello", "Hello");
     }
+    @Test
+    public void testTwo(){
+        assertEquals("Hello", "Hello");
+    }
+    @Test
+    public void testThree(){
+        assertEquals("Hello", "Hello");
+    }
+    @Test
+    public void testFour(){
+        assertEquals("Hello", "Hello");
+    }
+    @Test
+    public void testFive(){
+        assertEquals("Hello", "Hello");
+    }
+    @Test
+    public void testSix(){
+        assertEquals("Hello", "Hello");
+    }
+    @Test
+    public void testSeven(){
+        assertEquals("Hello", "Hello");
+    }
+    @Test
+    public void testEight(){
+        assertEquals("Hello", "Hello");
+    }
+    @Test
+    public void testNine(){
+        assertEquals("Hello", "Hello");
+    }
+    @Test
+    public void testTen(){
+        assertEquals("Hello", "Hello");
+    }
 }
